b0109f10cd520d55f509bbe5df2e2a54f53bc906
Rosario Díaz <46304949+ChayChaio@users.noreply.github.com>
2024-10-29 20:13:06 -0300

    Update Principal.java

--- screenmatch/src/main/java/com/aluracursos/screenmatch/principal/Principal.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -12,7 +12,7 @@ public class Principal {
     private Scanner lectura = new Scanner(System.in);
     private ConsumoAPI consumoApi = new ConsumoAPI();
     private final String URL_BASE = "https://www.omdbapi.com/?t=";
-    private final String API_KEY = "&apikey=2d094555";
+    private final String API_KEY = "TU-API-KEY";
     private ConvierteDatos conversor = new ConvierteDatos();
     private List<DatosSerie> datosSeries = new ArrayList<>();
     private SerieRepository repositorio;
